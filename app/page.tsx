6bea2f90cb9c67b00a40db84da78d640b5cd0dbe
unpixelz <sokostoko@gmail.com>
2026-01-10 18:10:59 +0100

    changed layout config, initial login screen commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,19 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          src="https://upload.wikimedia.org/wikipedia/commons/9/92/Markus_Ruhl.png"
           alt="Next.js logo"
-          width={100}
-          height={20}
+          width={300}
+          height={40}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            TestCommit
+          <h1 className="max-w-xs text-4xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Dashboard under construction.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="max-w-sm text-lg leading-7 text-zinc-600 dark:text-zinc-400">
+            This admin dashboard is currently being built. Stay tuned for
+            updates and new features!
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
