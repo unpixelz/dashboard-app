c46a5d8563414cadbb0314dd66f0c5d14cc5a75f
unpixelz <sokostoko@gmail.com>
2026-01-12 15:35:21 +0100

    supabase authentication logic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert"
 import { LoginForm } from "@/components/login-form";
 import Spline from "@splinetool/react-spline";
-
-/* Login Seite mit 3D Hintergrund und Login Formular */
+import { useState } from "react";
+import { Terminal } from "lucide-react";
 
 export default function LoginPage() {
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
   return (
     <div className="bg-black flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="absolute inset-0 z-0 blur-xl">
@@ -12,8 +18,17 @@ export default function LoginPage() {
           className="w-full h-full"
         />
       </div>
+      <div className="z-10 w-full max-w-sm animate-out fade-out delay-1000">
+        {errorMessage && (
+          <Alert variant="destructive" className="mb-4">
+            <Terminal />
+            <AlertTitle>Login Error</AlertTitle>
+            <AlertDescription>{errorMessage}</AlertDescription>
+          </Alert>
+        )}
+      </div>
       <div className="w-full max-w-sm flex flex-col items-center gap-6 z-10">
-        <LoginForm />
+        <LoginForm setErrorMessage={setErrorMessage} />
       </div>
     </div>
   );
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,5 +1,7 @@
-import { Hexagon } from "lucide-react";
+"use client";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { ChartNoAxesCombined, Terminal } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,20 +12,54 @@ import {
   FieldSeparator,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { use } from "react";
-import { userInfo } from "os";
-import { userAgent } from "next/server";
-
-
-const username = "Tim";
+import { createClient } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function LoginForm({
   className,
+  setErrorMessage, // destructured, wird NICHT in ...props landen
   ...props
-}: React.ComponentProps<"div">) {
+}: React.ComponentProps<"div"> & {
+  setErrorMessage?: (msg: string | null) => void;
+}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  //const [error, setError] = useState<string | null>(null);
+
+  const supabase = createClient();
+  const router = useRouter();
+
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+
+    setLoading(true);
+    setErrorMessage?.(null);
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      setErrorMessage?.(error.message);
+    } else {
+      router.push("/dashboard");
+    }
+
+    setLoading(false);
+  }
+
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
+    <div
+      className={cn(
+        "flex flex-col gap-6 animate-in fade-in duration-1000 delay-500",
+        className
+      )}
+      {...props} // nur restliche HTML-Props
+    >
+      <form onSubmit={handleLogin}>
         <FieldGroup>
           <div className="flex flex-col items-center gap-2 text-center">
             <a
@@ -31,31 +67,40 @@ export function LoginForm({
               className="flex flex-col items-center gap-2 font-medium pb-10"
             >
               <div className="flex size-30  items-center justify-center rounded-md">
-                <Hexagon className="size-30" />
+                <ChartNoAxesCombined className="size-30" />
               </div>
             </a>
-            <h1 className="text-xl font-bold">Willkommen zurück, {username}</h1>
+            <h1 className="text-xl font-bold">Willkommen zurück, tim</h1>
           </div>
           <div className="grid place-items-center">
             <Field>
-              <FieldLabel htmlFor="username">Username</FieldLabel>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
               <Input
-                id="username"
-                type="text"
-                placeholder="username"
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="email"
                 required
               />
               <FieldLabel htmlFor="password">Password</FieldLabel>
               <Input
                 id="password"
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="••••••••"
                 required
               />
             </Field>
           </div>
           <Field>
-            <Button className="transition active:scale-95 hover:bg-zinc-350" type="submit">Login</Button>
+            <Button
+              className="transition active:scale-95 hover:bg-zinc-350"
+              type="submit"
+            >
+              Login
+            </Button>
           </Field>
         </FieldGroup>
       </form>
